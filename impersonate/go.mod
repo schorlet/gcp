795c0927dba18b6411a00f92bd3eee161658c6c1
stephanec <stephanec@washingmachine>
2020-01-30 21:52:29 +0100

    short-lived credentials

---
+++ impersonate/go.mod
@@ -0,0 +1,9 @@
+module github.com/schorlet/gcp/impersonate
+
+go 1.13
+
+require (
+	cloud.google.com/go v0.52.0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/genproto v0.0.0-20200128133413-58ce757ed39b
+)
---
+++ impersonate/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"golang.org/x/oauth2"
+
+	credentials "cloud.google.com/go/iam/credentials/apiv1"
+	credentialspb "google.golang.org/genproto/googleapis/iam/credentials/v1"
+)
+
+var (
+	functionURL         = flag.String("url", "", "the function url to invoke")
+	impersonatedAccount = flag.String("account", "", "the service account for which the credentials are requested")
+)
+
+func main() {
+	flag.Parse()
+	if flag.NFlag() != 2 {
+		flag.Usage()
+		os.Exit(-2)
+	}
+
+	ctx := context.Background()
+	c, err := credentials.NewIamCredentialsClient(ctx)
+	if err != nil {
+		log.Fatalf("new credentials client: %v", err)
+	}
+	defer c.Close()
+
+	req := &credentialspb.GenerateIdTokenRequest{
+		Name:         "projects/-/serviceAccounts/" + *impersonatedAccount,
+		Audience:     *functionURL,
+		IncludeEmail: true,
+	}
+
+	resp, err := c.GenerateIdToken(ctx, req)
+	if err != nil {
+		log.Fatalf("generate oidc token: %v", err)
+	}
+
+	body, err := invoke(resp.Token)
+	if err != nil {
+		log.Fatalf("invoke: %v", err)
+	}
+
+	fmt.Println(body)
+}
+
+func invoke(token string) (string, error) {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	ctx := context.Background()
+
+	client := oauth2.NewClient(ctx, ts)
+	resp, err := client.Get(*functionURL)
+	if err != nil {
+		return "", fmt.Errorf("client get: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("read response: %v", err)
+	}
+
+	return string(body), nil
+}
