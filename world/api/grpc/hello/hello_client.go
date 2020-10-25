e3bf00f81ffa41ff59670b5ca87cc75d05938963
stephanec <stephanec@washingmachine>
2020-10-25 20:09:38 +0000

    client side load balancing

--- world/api/grpc/hello/hello_client.go
+++ world/api/grpc/hello/hello_client.go
@@ -40,6 +40,11 @@ func NewClient(addr string) (*Client, error) {
 		opts = append(opts, grpc.WithInsecure())
 	}
 
+	opts = append(opts, grpc.WithDefaultServiceConfig(
+		`{"loadBalancingPolicy":"round_robin"}`,
+	))
+	addr = fmt.Sprintf("dns:%s", addr)
+
 	ctx, timeout := context.WithTimeout(context.Background(), 3*time.Second)
 	defer timeout()
 
